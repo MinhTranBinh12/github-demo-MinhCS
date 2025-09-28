4f9a171778d9ca7ff3e6851e8916cde33ae14aa8
MinhTranBinh12 <minhtranbinh01102006@gmail.com>
2025-09-28 19:06:43 +0700

    Add index.cpp file

---
+++ index.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define For(i,n,m) for(int i=n; i<=m; ++i)
+#define ForD(i,m,n) for(int i=m; i>=n; --i)
+#define Lwb(a,x) lower_bound(a.begin(), a.end(), x) - a.begin()
+#define Upb(a,x) upper_bound(a.begin(), a.end(), x) - a.begin()
+#define Fill(a,s) memset(a,s,sizeof(a))
+#define endl '\n'
+#define name "main."
+using namespace std;
+typedef pair<int,int> ii;
+typedef long long ll;
+typedef double db;
+const ll maxn = 1e4+4;
+const ll maxN = 1e8+8;
+int a;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout << "bay acc git";
+    return 0;
+}
